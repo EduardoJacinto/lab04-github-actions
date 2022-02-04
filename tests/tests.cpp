c918a8113097963e885614a0282816f102d61c67
zamyy78 <zamyy78@gmail.com>
2022-02-03 17:14:15 -0800

    Modify unit tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -9,12 +9,35 @@ TEST(RectangleTests, CheckRectangle) {
 
 }
 
+TEST(RectangleTests, CheckRectangle2) {
+	Rectangle r(10,10);
+	EXPECT_EQ(r.get_width(), 10);
+	EXPECT_EQ(r.get_height(), 10);
 
+}
 
-TEST(AreaTests, Area) {
+TEST(RectangleTests, CheckRectangle3) {
+	Rectangle r(100, 100);
+	EXPECT_EQ(r.get_width(), 100);
+	EXPECT_EQ(r.get_height(), 100);
+}
+
+TEST(AreaTests, CheckArea2) {
 	Rectangle r(4,4);
 	int areaRes = r.area();
 	EXPECT_EQ(areaRes, 16);
+}
+
+TEST(AreaTests, CheckArea2) {
+	Rectangle r(10,10);
+	int areaRes = r.area();
+	EXPECT_EQ(areaRes, 100);
+}
+
+TEST(AreaTests, CheckArea3) {
+	Rectangle r(100,100);
+	int areaRes = r.area();
+	EXPECT_EQ(areaRes, 10000);
 }
 
 TEST(PerimeterTests, Perimeter) {
@@ -22,6 +45,19 @@ TEST(PerimeterTests, Perimeter) {
 	int perimRes = r.perimeter();
 	EXPECT_EQ(perimRes, 16);
 
+}
+
+TEST(PerimeterTests, Perimeter2) {
+	Rectangle r(10,10);
+	int perimRes = r.perimeter();
+	EXPECT_EQ(perimRes, 40);
+
+}
+
+TEST(PerimeterTests, Perimeter3) {
+	Rectangle r(100,100);
+	int perimRes = r.peimeter();
+	EXPECT_EQ(perimRes, 400);
 }
 
 int main(int argc, char **argv) {
